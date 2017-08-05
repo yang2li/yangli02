1ca7d75a6a2901e342d5aa3029118a83e15ac3d6
admin <393544233@qq.com>
2017-08-05 15:09:03 +0800

    Initial commit

---
+++ src/main/java/cn/bdqn/tangcco/tcmp056/test1/TestHelloWorld.java
@@ -0,0 +1,13 @@
+package cn.bdqn.tangcco.tcmp056.test1;
+
+import org.junit.Test;
+
+/**
+ * Created by admin on 2017/8/5.
+ */
+public class TestHelloWorld {
+    @Test
+    public static void main(String[] args) {
+        System.out.println("HelloWorld!");
+    }
+}
